e24908a6a6c66e127d63c6f3314e5e4e61275130
Spyro (Home) <epicdudeness677@gmail.com>
2022-07-16 23:37:28 +0200

    Hidden a variable.

--- Assets/Scripts/Environment/MovableObject.cs
+++ Assets/Scripts/Environment/MovableObject.cs
@@ -7,7 +7,7 @@ public class MovableObject : MonoBehaviour
     [Range(0, 1)] public float currentT;
     public float speed = 1;
 
-    public bool looping = false;
+    [HideInInspector] public bool looping = false;
 
 
     private float[] tValues;
@@ -20,29 +20,36 @@ public class MovableObject : MonoBehaviour
 
     [HideInInspector] public Vector3 positionOnStart;
 
-    private void Start() {
+    private void Start()
+    {
         rig = GetComponent<Rigidbody2D>();
 
         tValues = new float[path.Length - 1];
         distSum = 0;
-        for (int p = 0; p < path.Length - 1; p++) {
+        for (int p = 0; p < path.Length - 1; p++)
+        {
             tValues[p] = Vector3.Distance(path[p], path[p + 1]);//save distances in the tValues
             distSum += tValues[p];
         }
 
-        for(int t = 0; t < tValues.Length; t++) {
+        for (int t = 0; t < tValues.Length; t++)
+        {
             tValues[t] /= distSum;//divide every distance by the sum of distances to normalize them
         }
+        positionOnStart = transform.position;
     }
 
-    private void MoveToPos(Vector2 pos, float time) {
+    private void MoveToPos(Vector2 pos, float time)
+    {
         rig.velocity = (pos - rig.position) / time;//v = s / t
     }
 
-    private Vector2 GetPathPos(float t) {
-        for(; pathI < tValues.Length; pathI++) {
+    private Vector2 GetPathPos(float t)
+    {
+        for (; pathI < tValues.Length; pathI++)
+        {
             if (t < pathSum + tValues[pathI])//t value is between these points
-                return Vector2.Lerp(path[pathI], path[pathI + 1], (t- pathSum) /tValues[pathI]);//calculate scaled t Value
+                return Vector2.Lerp(path[pathI], path[pathI + 1], (t - pathSum) / tValues[pathI]);//calculate scaled t Value
             pathSum += tValues[pathI];
         }
         Debug.LogError("Path Position Calculation failed");
@@ -50,7 +57,8 @@ public class MovableObject : MonoBehaviour
     }
 
     private float DistToPercent(float dist) { return dist / distSum; }
-    private void MoveAlongPath(float speed, float deltatime) {
+    private void MoveAlongPath(float speed, float deltatime)
+    {
         float dt = DistToPercent(speed * deltatime);//get the change in t
         currentT += dt;
         Vector2 pos = GetPathPos(currentT);
